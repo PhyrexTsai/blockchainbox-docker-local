d5f2915865fe0e79dc2a1125526ee0e9ba6aa7e0
Yung chieh Tsai <x01ep23i@hotmail.com>
2016-12-25 00:11:17 +0800

    fix contract

--- ethereum/contract/ProofOfTransaction.sol
+++ ethereum/contract/ProofOfTransaction.sol
@@ -1,26 +1,26 @@
-pragma solidity ^0.4.7;
+pragma solidity ^0.4.6;
 
 contract ProofOfTransaction {
 
-  mapping (address => byte32) dataHashMap; // dataHash = sha3(data)  
+  mapping (address => bytes32) dataHashMap;
 
   // 寫入時記錄誰將資料存入
-  event sendDataHashedEvent(byte32 dataHash, address from, uint time);
+  event sendDataHashedEvent(bytes32 dataHash, address from, uint time);
   // 當有人查詢 dataHash 的時候紀錄下來
-  event sendIsDataHashExistEvent(byte32 dataHash, address from, uint time, bool exist);
+  event sendIsDataHashExistEvent(bytes32 dataHash, address from, uint time, bool exist);
   
   function setData(address from, string data) {
     // 將資料透過 SHA3 運算後存入 dataArray 中
-    byte32 dataHash = sha3(data);
+    bytes32 dataHash = sha3(data);
     dataHashMap[tx.origin] = dataHash;
     
     // 可以透過 send event 讓 web3.js 去接收，有這個就可以直接把 isExist 忽略
-    sendDataHashedEvent(dataHash, from, now);
+    sendDataHashedEvent(dataHashMap[tx.origin], from, now);
   }
   
-  function getDataHash(address txHash) returns (byte32) {
+  function getDataHash(address txHash) returns (bytes32) {
     // 透過 txHash 取得儲存的 dataHash
-    byte32 dataHash = dataHashMap[txHash];
+    bytes32 dataHash = dataHashMap[txHash];
     if (dataHash == 0) {
       return "";
     } else {
@@ -28,12 +28,12 @@ contract ProofOfTransaction {
     } 
   }
   
-  function isDataHashExist(address txHash, byte32 dataHash) returns (bool) {
+  function isDataHashExist(address txHash, bytes32 dataHash) returns (bool) {
     // 確認是否 dataHash 存在
     bool exist = false;
     
     // do something
-    byte32 storedDataHash = dataHashMap[txHash];
+    bytes32 storedDataHash = dataHashMap[txHash];
     if (dataHash == storedDataHash) {
       exist = true;
     }
